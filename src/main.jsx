54decbe1faec0a0fab69037c246aba19c6e9d302
kkchen <alie40521@gmail.com>
2026-01-15 00:38:59 +0800

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './assets/all.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/z-client">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
